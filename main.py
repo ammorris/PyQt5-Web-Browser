bd85bb0caddebc34184a66e29993fb4b61783e16
Andrea Morris <37010476+ammorris@users.noreply.github.com>
2022-06-14 18:46:51 -0700

    Added url bar and button functionality.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import *
 
 
-class MyWebBrowser(QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super(MyWebBrowser, self).__init__(*args, **kwargs)
+class MyWebBrowser:
+    def __init__(self):
 
         self.window = QWidget()
         self.window.setWindowTitle("Guaranteed Good Shit")
@@ -26,14 +24,16 @@ class MyWebBrowser(QMainWindow):
         self.forward_btn = QPushButton(">")
         self.forward_btn.setMinimumHeight(30)
 
-        #--------------------------------------------------------
-
         self.horizontal.addWidget(self.url_bar)
         self.horizontal.addWidget(self.go_btn)
         self.horizontal.addWidget(self.back_btn)
         self.horizontal.addWidget(self.forward_btn)
 
         self.browser = QWebEngineView()
+
+        self.go_btn.clicked.connect(lambda: self.navigate(self.url_bar.toPlainText()))
+        self.back_btn.clicked.connect(self.browser.back)
+        self.forward_btn.clicked.connect(self.browser.forward)
 
         self.layout.addLayout(self.horizontal)
         self.layout.addWidget(self.browser)
@@ -42,6 +42,12 @@ class MyWebBrowser(QMainWindow):
 
         self.window.setLayout(self.layout)
         self.window.show()
+
+    def navigate(self, url):
+        if not url.startswith("http"):
+            url = "http://" + url
+            self.url_bar.setText(url)
+        self.browser.setUrl(QUrl(url))
 
 
 app = QApplication([])
